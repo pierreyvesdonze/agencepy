35a09b75549b5554ad29164b6ff3a5bbc4775a1d
py <pyd3.14@gmail.com>
2020-12-18 19:12:31 +0100

    Ajout d'articles en session

--- src/Controller/WitchController.php
+++ src/Controller/WitchController.php
@@ -17,32 +17,31 @@ class WitchController extends AbstractController
      */
     public function witchHome(): Response
     {
-        return $this->render('witch/index.witch.html.twig', [
-
-        ]);
+        return $this->render('witch/index.witch.html.twig', []);
     }
 
-     /**
+    /**
      * @Route("/witch/shop", name="witch_shop", methods={"GET","POST"})
      */
     public function witchShop(
         WitchProductRepository $witchProductRepository,
         Request $request
-        ) 
-    {
+    ) {
         $products = $witchProductRepository->findAll();
 
-        $formatForm = $this->createForm(WitchFormatType::class);
-        $formatForm->handleRequest($request);
-
-        if ($formatForm->isSubmitted() && $formatForm->isValid()) {
-
-            $this->addFlash('success', 'Votre article a bien été ajouté au panier');
+        if (isset($_POST)) {
+            $session = $request->getSession();
+            $articlesArray = $session->get('newArticle');
+            if ($articlesArray == null){
+              $articlesArray = [];
+            }
+            $articlesArray[] = $_POST;
+            $session->set('newArticle', $articlesArray);
+            dump($session);
         }
 
         return $this->render('witch/shop.witch.html.twig', [
             'products' => $products,
-            'formatForm' => $formatForm->createView()
         ]);
     }
 }
